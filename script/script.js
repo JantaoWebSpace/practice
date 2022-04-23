c118533fdd8758c6ff29a263d9181b80ced55d4f
JantaoWebSpace <94252814+JantaoWebSpace@users.noreply.github.com>
2022-04-23 11:04:32 +0300

    change theme button and remove <p>

--- script/script.js
+++ script/script.js
@@ -1,11 +1,10 @@
 const button = document.querySelector('button')
-const label = document.querySelector('.theme')
 const body = document.querySelector('body')
 
 button.addEventListener('click', e => {
-   label.innerText === 'DARK'
-      ? (label.innerText = 'LIGHT')
-      : (label.innerText = 'DARK')
+   button.innerText === 'DARK theme'
+      ? (button.innerText = 'LIGHT theme')
+      : (button.innerText = 'DARK theme')
    button.classList.toggle('dark')
    body.classList.toggle('dark')
 })
